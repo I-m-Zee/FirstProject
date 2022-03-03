9c7a14d0b8d0050de85bd8fd626e2cded8c381d9
bc090402389@gmail.com <bc090402389@gmail.com>
2022-03-03 20:26:30 +0500

    Adding Service

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,9 +1,24 @@
 import { Component } from "@angular/core";
+import { CourseService } from "./course/course.service";
 
 @Component({
     selector:'courses',
-    template:'<h2>Courses</h2>'
+    template:`<h2>{{getTitle()}}</h2>
+    <ul>
+    <li *ngFor="let course of courses">
+    {{course}}
+    </li>
+    </ul>
+    `
 })
 export class CoursesComponent{
+    title="List of Courses"
+    courses;
+    getTitle(){
+        return "Title: "+this.title;
+    }
+    constructor(service:CourseService){
+        this.courses = service.getCourse();
 
+    }
 }
