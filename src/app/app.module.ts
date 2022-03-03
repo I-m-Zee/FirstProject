9726cbaa741ada31e6d8517c155b9b26c1089e6a
bc090402389@gmail.com <bc090402389@gmail.com>
2022-03-03 20:26:47 +0500

    Merged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
+import { CourseService } from './course/course.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { CourseComponent } from './course/course.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CourseService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/course/course.service.ts
@@ -0,0 +1,5 @@
+export class CourseService{
+    getCourse(){
+        return ['Course1', 'course2', 'course3'];
+    }
+}
