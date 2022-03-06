8a093014f8e4bac2b7ba0df79f8f4c289a9d65d1
bc090402389@gmail.com <bc090402389@gmail.com>
2022-03-06 13:57:49 +0500

    Added Bootstrap

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,16 +3,32 @@ import { CourseService } from "./course/course.service";
 
 @Component({
     selector:'courses',
-    template:`<h2>{{getTitle()}}</h2>
+    template:
+    `
+    <h2>{{getTitle()}}</h2>
+    <h2 [textContent]="getTitle()"></h2>
     <ul>
     <li *ngFor="let course of courses">
     {{course}}
     </li>
     </ul>
+    <img src="{{imageUrl}}"/>
+    <img [src]="imageUrl"/>
+    <table>
+    <tr>
+    <td [attr.colspan]="colSpan">
+    </td>
+    </tr>
+
+    </table>
+
+    <button class="btn btn-primary">Save</button>
     `
 })
 export class CoursesComponent{
-    title="List of Courses"
+    title="List of Courses";
+    imageUrl="~/test.jpg";
+    colSpan=2;
     courses;
     getTitle(){
         return "Title: "+this.title;
